af4d7ba0a78d8f792efbfe57e221e779ae4efdef
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-26 14:58:02 +0900

    reposirtory追加

---
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -0,0 +1,59 @@
+package com.example.repository;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Comment;
+
+@Repository
+public class CommentRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Comment> COMMENT_ROW_MAPPER
+	= (rs,i) -> {
+		Comment comment = new Comment();
+		comment.setId(rs.getInt("id"));
+		comment.setName(rs.getString("name"));
+		comment.setContent(rs.getString("content"));
+		comment.setArticleId(rs.getInt("article_id"));
+		
+		return comment;
+	};
+	
+	public List<Comment> findByArticleId(int articleId){
+		String sql = "SELECT id, name, content, article_id FROM comments WHERE article_id=:articleId";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("article_id", articleId);
+		
+		List<Comment> commentList = template.query(sql, param, COMMENT_ROW_MAPPER);
+		
+		return commentList;
+	}
+	
+	public void insert(Comment comment) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(comment);
+		String sql = "INSERT INTO comments(name, content, article_id) VALUES(:name, :content, :articleId)";
+		
+		template.update(sql, param);
+	}
+	
+	public void deleteByArticleId(int articleId) {
+		String sql = "DELETE FROM comments WHERE article_id=:articleId";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("article_id",articleId);
+		
+		template.update(sql, param);
+	}
+	
+	
+}
